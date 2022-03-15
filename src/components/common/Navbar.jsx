ec0cf58819b6f817298d582e648e7ed352faa080
abdelalishare <abdelali.kabou@shareconseil.com>
2022-03-15 23:44:20 +0100

    Home page v0.1 - add assets - add Navbar - add `NavbarItems.json` - add some style - add custom `useFetch` to loading data - add pagination - loading & errors handling

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavItems from "./lists/NavItems";
+
+const Navbar = ({logoSrc, alt,navItems}) => {
+    return (
+    <div className="navbar">
+        <img src={logoSrc} alt={alt}/>
+        <NavItems items={navItems}/>
+    </div>
+)
+};
+
+export default Navbar;
